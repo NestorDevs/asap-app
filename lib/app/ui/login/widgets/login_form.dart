88351f98f58a3c2942273b31c5c89d6e052d64e8
nestorDev2021 <81179663+nestorDev2021@users.noreply.github.com>
2021-04-10 19:52:41 -0400

    add google login

--- lib/app/ui/login/widgets/login_form.dart
+++ lib/app/ui/login/widgets/login_form.dart
@@ -1,6 +1,7 @@
 import 'package:asap_app/app/ui/widgets/image_landing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:formz/formz.dart';
 
 import '../../singup/signup_page.dart';
@@ -32,6 +33,8 @@ class LoginForm extends StatelessWidget {
               _PasswordInput(),
               const SizedBox(height: 8.0),
               _LoginButton(),
+              const SizedBox(height: 8.0),
+              _GoogleLoginButton(),
               const SizedBox(height: 8.0),
               _SignUpButton(),
             ],
@@ -116,6 +119,28 @@ class _LoginButton extends StatelessWidget {
                 ),
               );
       },
+    );
+  }
+}
+
+class _GoogleLoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return ElevatedButton.icon(
+      key: const Key('loginForm_googleLogin_raisedButton'),
+      label: const Text(
+        'SIGN IN WITH GOOGLE',
+        style: TextStyle(color: Colors.white),
+      ),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        primary: theme.accentColor,
+      ),
+      icon: const Icon(FontAwesomeIcons.google, color: Colors.white),
+      onPressed: () => context.read<LoginCubit>().logInWithGoogle(),
     );
   }
 }
